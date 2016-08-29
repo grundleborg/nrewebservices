ab0ac52157f487eff60ad6f7963454194d0b5462
George Goldberg <george@gberg.me>
2016-08-29 20:31:50 +0100

    Add get_station_board_with_details method.

--- nrewebservices/ldbws/webservice.py
+++ nrewebservices/ldbws/webservice.py
@@ -146,4 +146,88 @@ class Session(object):
         soap_response = query(**params)
         return StationBoard(soap_response)
 
+    def get_station_board_with_details(self, crs, rows=10, include_departures=True,
+            include_arrivals=False, from_filter_crs=None, to_filter_crs=None, time_offset=None,
+            time_window=None):
+        """
+        Get a list of public services at a station as would populate a departure/arrival board.
+        This method is identical in arguments and result to  `get_station_board`, except that the
+        returned result is of type `StationBoardWithDetails`, which includes the calling points
+        on the services, allowing access to them without an additional call to `get_service_details`
+        for each service.
+
+        Args:
+            crs (str): the CRS code of the station for which this board is being fetched.
+
+            rows (int, from 1 to 150): the maximum number of services to include in the returned
+                board.
+
+            include_departures (boolean): whether the returned services should include departures
+                from this station. At least one of `include_departures` or `include_arrivals` must
+                be set to true.
+
+            include_arrivals (boolean): whether the returned services should include arrivals at
+                this station. At least one of `include_departures` or `include_arrivals` must be set
+                to true.
+
+            from_filter_crs (str): the CRS code of a station at which all services returned must
+                have called previously. Only one of `from_filter_crs` and `to_filter_crs` can be set
+                for a given request.
+
+            to_filter_crs (str): the CRS code of a station at which all services returned must
+                subsequently call. Only one of `from_filter_crs` and `to_filter_crs` can be set for
+                a given request.
+
+            time_offset (int, from -120 to 120): An offset in minutes against the current time which
+                determines the starting point of the time window for which services are returned. If
+                set to `None`, the value of 0 will be used.
+
+            time_window (int, from -120 to 120): How far into the future from the value passed as
+                `time_offset` should services be fetched. If the value passed is negative, the time
+                window starts before the value of `time_offset` and ends at `time_offset`. If `None`
+                is passed, the default value is 120.
+
+        Returns:
+            StationBoardWithDetails: a `StationBoardWithDetails` object containing the station
+            details and the requested services, along with their calling points.
+
+        Raises:
+            ValueError: if neither include_departures or include_arrivals are set to True.
+
+        Note:
+            Each time this his method is called, it makes **1** request to the LDBWS server.
+        """
+
+        # Get the appropriate SOAP query method.
+        if include_departures and include_arrivals:
+            query = self._service.GetArrDepBoardWithDetails
+        elif include_departures:
+            query = self._service.GetDepBoardWithDetails
+        elif include_arrivals:
+            query = self._service.GetArrBoardWithDetails
+        else:
+            raise ValueError("When calling get_station_board, either include_departures or include_arrivals must be set to True.")
+
+        # Construct the query parameters.
+        params = {}
+        params['crs'] = crs
+        params['numRows'] = rows
+        if to_filter_crs:
+            if from_filter_crs:
+                log.warn("get_station_board() can only be filtered on one of from_filter_crs and to_filter_crs. Since both are provided, using only to_filter_crs")
+            params['filterCrs'] = to_filter_crs
+            params['filterType'] = 'to'
+        elif from_filter_crs:
+            params['filterCrs'] = from_filter_crs
+            params['filterType'] = 'from'
+        if time_offset is not None:
+            params['timeOffset'] = time_offset
+        if time_window is not None:
+            params['timeWindow'] = time_window
+
+        # Do the SOAP query.
+        # TODO: Some form of error handling.
+        soap_response = query(**params)
+        return StationBoard(soap_response)
+
 
