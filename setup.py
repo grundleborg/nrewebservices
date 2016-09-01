d9e0f8b4e17dcaf9d660ebef821b5953c30016c9
George Goldberg <george@gberg.me>
2016-09-01 17:45:20 +0100

    Make package installable.

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+
+from setuptools import setup, find_packages
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='nrewebservices',
+    version='0.1.0-dev',
+    packages=find_packages(),
+    description='A Python 2/3 library for accessing the National Rail Enquiries web service APIs.',
+    long_description=(read('README.md')),
+    url='https://github.com/grundleborg/nrewebservices',
+    license='BSD',
+    author='George Goldberg',
+    author_email='george@gberg.me',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[ read('requirements.txt').split('\n') ],
+)
